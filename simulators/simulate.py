2efe34d2f8bf9fd83b9a0f4e8877e3932ee3ce25
Karandeep Singh Nagra <karandeepsnagra@gmail.com>
2013-07-01 14:24:46 -0700

    Create simulate.py

---
+++ simulators/simulate.py
@@ -0,0 +1,9 @@
+# Author: Karandeep Singh Nagra
+
+'''
+An adaptation of Iwan Smith's simulate.m to Python.  Instead of piping the output of main.c into krackoutput,
+send it to a file named out:
+./main > out
+
+Run this script in the same folder as the file out, and a window will pop up with an animation of the board.
+'''
