2f011b2b25bb3368021561e40ad06a052544fd0c
Karandeep Singh Nagra <karandeepsnagra@gmail.com>
2014-01-16 14:40:35 -0800

    Update simulate.py
    It seems the board is rotated 180 degrees clockwise, as with Iwan's code.

--- simulators/simulate.py
+++ simulators/simulate.py
@@ -3,8 +3,10 @@
 '''
 An adaptation of Iwan Smith's simulate.m to Python.  Instead of piping the output of main.c into krackoutput,
 send it to a file named out:
-./main > out
+gcc main.c <pixel_color_implementation.c>
+./a.out > out
 
+Wait a few seconds to load up a bunch of lines to out.
 Run this script in the same folder as the file out, and a window will pop up with an animation of the board.
 '''
 
