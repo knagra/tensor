3167c48c47b7c47d3e1b88a0916c73e5c94a1ea1
Karandeep Singh Nagra <karandeepsnagra@gmail.com>
2014-01-16 14:24:42 -0800

    Update simulate.py
    Displays half of screen now.

--- simulators/simulate.py
+++ simulators/simulate.py
@@ -37,6 +37,9 @@ blue_image = zeros (24, 48) )
 
 def pos(x):
   return (x * RADIUS * 2) + RADIUS
+  
+def true_color(r, g, b):
+  return (r * 16, g * 16, b * 16)
 
 while 1:
   screen.fill(BLACK)
@@ -50,18 +53,18 @@ while 1:
     green_data[panel, : ] = linedata[67:130]
     blue_data[panel, : ] = linedata[131:194]
     
-  panel = 1
+  panel = 0
   for pan_hor in range(6):
     for pan_ver in range(3):
-      panel = pan_hor + 1 + (pan_ver * 6)
+      panel = pan_hor + (pan_ver * 6)
       for line in range(8):
-        red_image[pan_ver*8+line+1, range(pan_hor*8+1, pan_hor*8+8+1)] = red_data[panel, range(line*8+1, line*8+8+1)]
-        green_image[pan_ver*8+line+1, range(pan_hor*8+1, pan_hor*8+8+1)] = green_data[panel, range(line*8+1, line*8+8+1)]
-        blue_image[pan_ver*8+line+1, range(pan_hor*8+1, pan_hor*8+8+1)] = blue_data[panel, range(line*8+1, line*8+8+1)]
+        red_image[pan_ver*8+line, range(pan_hor*8, pan_hor*8+8)] = red_data[panel, range(line*8, line*8+8)]
+        green_image[pan_ver*8+line, range(pan_hor*8, pan_hor*8+8)] = green_data[panel, range(line*8, line*8+8)]
+        blue_image[pan_ver*8+line, range(pan_hor*8, pan_hor*8+8)] = blue_data[panel, range(line*8, line*8+8)]
   
   for x in range(24):
     for y in range(48):
-      pygame.draw.circle(screen, [pos(x), pos(y)], [red_image[x, y], green_image[x, y], blue_image[x, y], RADIUS)
+      pygame.draw.circle(screen, true_color(red_image[x, y], green_image[x, y], blue_image[x, y]), [pos(x), pos(y)], RADIUS)
     
   pygame.display.flip()
   frame = frame + 1
